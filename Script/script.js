03e862ec896c5afda95039c0140264b4da0ed316
YureshMajhi <yureshmee@gmail.com>
2022-11-13 12:26:28 +0545

    code complete

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,10 @@
 let circleTurn = false;
+let gameOn = true;
 
 const cells = document.querySelectorAll(".cell");
 const reset = document.querySelector(".reset");
+
+const result = document.querySelector(".result");
 
 // converting cells nodelist to array
 const cellsArr = Array.from(cells);
@@ -9,13 +12,73 @@ const cellsArr = Array.from(cells);
 // clicking on gameboard
 cells.forEach((cell) =>
   cell.addEventListener("click", function () {
-    if (cell.textContent == "") {
-      if (circleTurn) {
-        cell.textContent = "O";
-      } else {
-        cell.textContent = "X";
+    if (gameOn) {
+      if (cell.textContent == "") {
+        if (circleTurn) {
+          cell.textContent = "O";
+        } else {
+          cell.textContent = "X";
+        }
+        circleTurn = !circleTurn;
       }
-      circleTurn = !circleTurn;
+    }
+
+    if (
+      cellsArr[0].textContent == cellsArr[1].textContent &&
+      cellsArr[0].textContent == cellsArr[2].textContent &&
+      cellsArr[0].textContent != ""
+    ) {
+      result.textContent = `${cellsArr[0].textContent} wins`;
+      gameOn = false;
+    } else if (
+      cellsArr[3].textContent == cellsArr[4].textContent &&
+      cellsArr[3].textContent == cellsArr[5].textContent &&
+      cellsArr[3].textContent != ""
+    ) {
+      result.textContent = `${cellsArr[3].textContent} wins`;
+      gameOn = false;
+    } else if (
+      cellsArr[6].textContent == cellsArr[7].textContent &&
+      cellsArr[6].textContent == cellsArr[8].textContent &&
+      cellsArr[6].textContent != ""
+    ) {
+      result.textContent = `${cellsArr[6].textContent} wins`;
+      gameOn = false;
+    } else if (
+      cellsArr[0].textContent == cellsArr[3].textContent &&
+      cellsArr[0].textContent == cellsArr[6].textContent &&
+      cellsArr[0].textContent != ""
+    ) {
+      result.textContent = `${cellsArr[0].textContent} wins`;
+      gameOn = false;
+    } else if (
+      cellsArr[1].textContent == cellsArr[4].textContent &&
+      cellsArr[1].textContent == cellsArr[7].textContent &&
+      cellsArr[1].textContent != ""
+    ) {
+      result.textContent = `${cellsArr[1].textContent} wins`;
+      gameOn = false;
+    } else if (
+      cellsArr[2].textContent == cellsArr[8].textContent &&
+      cellsArr[2].textContent == cellsArr[5].textContent &&
+      cellsArr[2].textContent != ""
+    ) {
+      result.textContent = `${cellsArr[2].textContent} wins`;
+      gameOn = false;
+    } else if (
+      cellsArr[0].textContent == cellsArr[4].textContent &&
+      cellsArr[0].textContent == cellsArr[8].textContent &&
+      cellsArr[0].textContent != ""
+    ) {
+      result.textContent = `${cellsArr[0].textContent} wins`;
+      gameOn = false;
+    } else if (
+      cellsArr[2].textContent == cellsArr[4].textContent &&
+      cellsArr[2].textContent == cellsArr[6].textContent &&
+      cellsArr[2].textContent != ""
+    ) {
+      result.textContent = `${cellsArr[2].textContent} wins`;
+      gameOn = false;
     }
   })
 );
@@ -24,4 +87,6 @@ cells.forEach((cell) =>
 reset.addEventListener("click", function () {
   cells.forEach((cell) => (cell.textContent = ""));
   circleTurn = false;
+  gameOn = true;
+  result.textContent = "";
 });
