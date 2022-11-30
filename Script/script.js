1cb097085bfe46295607bfce72139dab289eee9a
YureshMajhi <yureshmee@gmail.com>
2022-11-30 18:37:41 +0545

    revisiting code upto 3

--- Script/script.js
+++ Script/script.js
@@ -1,25 +1,26 @@
-// Setting up the Gameboard Module
+// setting up gameboard object
 const gameBoardModule = (() => {
-  let gameBoard = ["X"];
+  let gameBoard = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
   return { gameBoard };
 })();
 
-// Setting up the displayControllerModule
-const displayControllerModule = (() => {
-  let testF = () => {
-    console.log("this is a test");
-  };
+// setting up display controller module
+const displayController = (() => {
+  let testF = () => console.log("this is a test to check the render option");
   return { testF };
 })();
 
-// Setting up the player factory function
-const createPlayer = (playerName, playerNumber, assignXO) => {
+// setting up PlayerInput feild using factory
+const player = (playerName, playerNumber, assignXO) => {
   const getPlayerName = () => playerName;
   return { getPlayerName, playerNumber, assignXO };
 };
 
-// setting up the render for array on screen
-const renderArrayToScreenModule = (() => {
+// setting up rendering module
+const renderGame = (() => {
   const box = document.querySelectorAll(".box");
+  for (let i = 0; i < gameBoardModule.gameBoard.length; i++) {
+    box[i].textContent = gameBoardModule.gameBoard[i];
+  }
   return {};
 })();
