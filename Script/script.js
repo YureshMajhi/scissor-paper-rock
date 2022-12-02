8c9b0320aead0f967cf1539365220ac9de715bc3
YureshMajhi <yureshmee@gmail.com>
2022-12-02 18:43:39 +0545

    added dataset and linked to gameBoard array

--- Script/script.js
+++ Script/script.js
@@ -11,26 +11,34 @@ const displayController = (() => {
 
   // Assigning X and O to players on click
   const box = document.querySelectorAll(".box");
+
+  let index = 0;
   box.forEach((boxes) => {
+    // Adding dataset to all box elements
+    boxes.dataset.linkedId = index;
+    index++;
+
+    // Setting click event on box element
     boxes.addEventListener("click", () => {
       if (boxes.textContent == "") {
         crossTurn = !crossTurn;
         boxes.textContent = changeXO(crossTurn);
+        gameBoardModule.gameBoard[boxes.dataset.linkedId] = boxes.textContent;
       }
     });
   });
-})();
 
-// Changing assign value i.e. X and O
-const changeXO = (crossTurn) => {
-  let sign = "";
-  if (crossTurn) {
-    sign = "X";
-  } else {
-    sign = "O";
-  }
-  return sign;
-};
+  // Changing assign value i.e. X and O
+  const changeXO = (crossTurn) => {
+    let sign = "";
+    if (crossTurn) {
+      sign = "X";
+    } else {
+      sign = "O";
+    }
+    return sign;
+  };
+})();
 
 // setting up PlayerInput feild using factory
 const player = (playerName, playerNumber, assignXO) => {
