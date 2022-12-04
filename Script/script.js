1932330107f8ef985c408849c563493961401dd6
YureshMajhi <yureshmee@gmail.com>
2022-12-04 13:54:09 +0545

    win logic added

--- Script/script.js
+++ Script/script.js
@@ -1,42 +1,62 @@
 // setting up gameboard object
 const gameBoardModule = (() => {
-  let gameBoard = [];
-  return { gameBoard };
+  const gameBoard = document.querySelectorAll(".box");
+  const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  return { gameBoard, WINNING_COMBINATIONS };
 })();
 
 // setting up display controller module
 const displayController = (() => {
+  // setting up controls
+  const X_CLASS = "X";
+  const CIRCLE_CLASS = "O";
+  let endgame = false;
+
   //choosing turn
   let crossTurn = false;
 
   // Assigning X and O to players on click
-  const box = document.querySelectorAll(".box");
-
-  let index = 0;
-  box.forEach((boxes) => {
-    // Adding dataset to all box elements
-    boxes.dataset.linkedId = index;
-    index++;
-
+  gameBoardModule.gameBoard.forEach((boxes) => {
     // Setting click event on box element
     boxes.addEventListener("click", () => {
-      if (boxes.textContent == "") {
-        crossTurn = !crossTurn;
-        boxes.textContent = changeXO(crossTurn);
-        gameBoardModule.gameBoard[boxes.dataset.linkedId] = boxes.textContent;
+      if (!endgame) {
+        if (boxes.textContent == "") {
+          // checking turn
+          crossTurn = !crossTurn;
+          const currentClass = crossTurn ? X_CLASS : CIRCLE_CLASS;
+
+          // displaying on Board
+          boxes.textContent = currentClass;
+
+          // Checking win using function and currentClass variable
+          boxes.classList.add(currentClass);
+          if (checkWin(currentClass)) {
+            console.log(`${currentClass} wins`);
+            endgame = true;
+          }
+        }
       }
     });
   });
 
-  // Changing assign value i.e. X and O
-  const changeXO = (crossTurn) => {
-    let sign = "";
-    if (crossTurn) {
-      sign = "X";
-    } else {
-      sign = "O";
-    }
-    return sign;
+  // Checking winning conditions
+  const checkWin = (currentClass) => {
+    return gameBoardModule.WINNING_COMBINATIONS.some((combination) => {
+      return combination.every((index) => {
+        return gameBoardModule.gameBoard[index].classList.contains(
+          currentClass
+        );
+      });
+    });
   };
 })();
 
